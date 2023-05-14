ab17a2bc782d168361d236e86db61a58c764ade5
yasidew <it21440922@my.sliit.lk>
2023-05-14 08:29:09 +0530

    changes to yasithanew

--- app/src/main/java/com/example/fintrack/activities/Income.kt
+++ app/src/main/java/com/example/fintrack/activities/Income.kt
@@ -1,123 +1,152 @@
 package com.example.fintrack.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.SearchView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fintrack.R
+import com.example.fintrack.adapters.ExpAdapter
+import com.example.fintrack.adapters.IncAdapter
+import com.example.fintrack.models.ExpenseModel
+import com.example.fintrack.models.IncomeModel
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_income.*
+import java.util.*
+import java.util.Locale.filter
+import kotlin.collections.ArrayList
+
+class Income : AppCompatActivity() {
+
+    private lateinit var incRecyclerView: RecyclerView
+    private lateinit var lbLoadingIP: TextView
+    private lateinit var incList: ArrayList<IncomeModel>
+    private lateinit var dbRef: DatabaseReference
+    private lateinit var searchView: SearchView
+  // private lateinit var lbsavingsvalIP: EditText
+
+    private var totalIncome: Double = 0.0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_income)
+
+//       val lbsavingsvalIP = findViewById<EditText>(R.id.lbsavingsvalIP)
 //
-//import android.content.Intent
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-//import android.text.Editable
-//import android.text.TextWatcher
-//import android.view.View
-//import android.widget.EditText
-//import android.widget.ImageView
-//import android.widget.TextView
-//import androidx.recyclerview.widget.LinearLayoutManager
-//import androidx.recyclerview.widget.RecyclerView
-//import com.example.fintrack.R
-//import com.example.fintrack.adapters.ExpAdapter
-//import com.example.fintrack.adapters.IncAdapter
-//import com.example.fintrack.models.ExpenseModel
-//import com.example.fintrack.models.IncomeModel
-//import com.google.firebase.database.*
-//import kotlinx.android.synthetic.main.activity_income.*
+//        lbsavingsvalIP.addTextChangedListener(object : TextWatcher {
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 //
-//class Income : AppCompatActivity() {
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
 //
-//    private lateinit var incRecyclerView: RecyclerView
-//    private lateinit var lbLoadingIP: TextView
-//    private lateinit var incList: ArrayList<IncomeModel>
-//    private lateinit var dbRef: DatabaseReference
-//  // private lateinit var lbsavingsvalIP: EditText
-//
-//    private var totalIncome: Double = 0.0
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_income)
-//
-////       val lbsavingsvalIP = findViewById<EditText>(R.id.lbsavingsvalIP)
-////
-////        lbsavingsvalIP.addTextChangedListener(object : TextWatcher {
-////            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-////
-////            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-////
-////            override fun afterTextChanged(s: Editable?) {
-////                // Update the total income based on the new value in lbsavingsvalIP
-////                totalIncome = s.toString().toDoubleOrNull() ?: 0.0
-////            }
-////        })
-//        val ivAddiconblueIP = findViewById<ImageView>(R.id.ivAddiconblueIP)
-//
-//
-//        ivAddiconblueIP.setOnClickListener{
-//            val intent= Intent(this, AddIncome::class.java)
-//            startActivity(intent)
-//        }
-//
-//        val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
-//
-//
-//        incRecyclerView = findViewById(R.id.rvIncomesIP)
-//        incRecyclerView.layoutManager = LinearLayoutManager(this)
-//        incRecyclerView.setHasFixedSize(true)
-//        lbLoadingIP =  findViewById(R.id.lbLoadingIP)
-//    //    lbsavingsvalIP = findViewById(R.id.lbsavingsvalIP)
-//        incList = arrayListOf<IncomeModel>()
-//
-//        getIncomeData()
-//    }
-//
-//    private fun getIncomeData(){
-//
-//        incRecyclerView.visibility = View.GONE
-//        lbLoadingIP.visibility = View.VISIBLE
-//
-//        dbRef = FirebaseDatabase.getInstance().getReference("Incomes")
-//
-//        dbRef.addValueEventListener(object : ValueEventListener{
-//            override fun onDataChange(snapshot: DataSnapshot){
-//
-//                incList.clear()
-//               // totalIncome = 0.0
-//                if(snapshot.exists()){
-//                    for(incSnap in snapshot.children) {
-//                        val incData = incSnap.getValue(IncomeModel::class.java)
-//                        incList.add(incData!!)
-//
-////                        val amount = incData!!.incAmount?.toDoubleOrNull()
-////                        if (amount != null) {
-////                            totalIncome += amount
-////                        }
-////
-//                    }
-//                    //lbsavingsvalIP.setText(totalIncome.toString())
-//                    val mAdapter = IncAdapter(incList)
-//                    incRecyclerView.adapter = mAdapter
-//
-//                    mAdapter.setOnItemClickListener(object : IncAdapter.onItemClickListener{
-//                        override fun onItemClick(position: Int){
-//                            val intent = Intent(this@Income,UpdateIncome::class.java)
-//
-//
-//                            intent.putExtra("incId", incList[position].incId)
-//                            intent.putExtra("incName", incList[position].incName)
-//                            intent.putExtra("incCategory", incList[position].incCategory)
-//                            intent.putExtra("incAmount", incList[position].incAmount)
-//                            intent.putExtra("incDate", incList[position].incDate)
-//                            startActivity(intent)
-//                        }
-//
-//                    })
-//
-//                    incRecyclerView.visibility = View.VISIBLE
-//                    lbLoadingIP.visibility = View.GONE
-//
-//                }
-//            }
-//
-//            override fun onCancelled(error: DatabaseError) {
-//                TODO("Not yet implemented")
+//            override fun afterTextChanged(s: Editable?) {
+//                // Update the total income based on the new value in lbsavingsvalIP
+//                totalIncome = s.toString().toDoubleOrNull() ?: 0.0
 //            }
 //        })
+        val ivAddiconblueIP = findViewById<ImageView>(R.id.ivAddiconblueIP)
+
+
+        ivAddiconblueIP.setOnClickListener{
+            val intent= Intent(this, AddIncome::class.java)
+            startActivity(intent)
+        }
+
+        val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
+
+
+        incRecyclerView = findViewById(R.id.rvIncomesIP)
+        incRecyclerView.layoutManager = LinearLayoutManager(this)
+        incRecyclerView.setHasFixedSize(true)
+        lbLoadingIP =  findViewById(R.id.lbLoadingIP)
+        searchView = findViewById(R.id.searchviewIP)
+    //    lbsavingsvalIP = findViewById(R.id.lbsavingsvalIP)
+        incList = arrayListOf<IncomeModel>()
+
+        getIncomeData()
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filter(newText)
+                return true
+            }
+        })
+
+    }
+
+    private fun filter(text: String?) {
+        text?.let {
+            val filteredList = incList.filter { income ->
+                income.incName?.toLowerCase(Locale.ROOT)?.contains(text.toLowerCase(Locale.ROOT)) == true
+            }
+            (incRecyclerView.adapter as IncAdapter).submitList(filteredList)
+        } ?: run {
+            (incRecyclerView.adapter as IncAdapter).submitList(incList)
+        }
+    }
+
+    private fun getIncomeData(){
+
+        incRecyclerView.visibility = View.GONE
+        lbLoadingIP.visibility = View.VISIBLE
+
+        dbRef = FirebaseDatabase.getInstance().getReference("Incomes")
+
+        dbRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot){
+
+                incList.clear()
+               // totalIncome = 0.0
+                if(snapshot.exists()){
+                    for(incSnap in snapshot.children) {
+                        val incData = incSnap.getValue(IncomeModel::class.java)
+                        incList.add(incData!!)
+
+//                        val amount = incData!!.incAmount?.toDoubleOrNull()
+//                        if (amount != null) {
+//                            totalIncome += amount
+//                        }
 //
-//    }
-//}
+                    }
+                    //lbsavingsvalIP.setText(totalIncome.toString())
+                    val mAdapter = IncAdapter(incList)
+                    incRecyclerView.adapter = mAdapter
+
+                    mAdapter.setOnItemClickListener(object : IncAdapter.onItemClickListener{
+                        override fun onItemClick(position: Int){
+                            val intent = Intent(this@Income,UpdateIncome::class.java)
+
+
+                            intent.putExtra("incId", incList[position].incId)
+                            intent.putExtra("incName", incList[position].incName)
+                            intent.putExtra("incCategory", incList[position].incCategory)
+                            intent.putExtra("incAmount", incList[position].incAmount)
+                            intent.putExtra("incDate", incList[position].incDate)
+                            startActivity(intent)
+                        }
+
+                    })
+
+                    incRecyclerView.visibility = View.VISIBLE
+                    lbLoadingIP.visibility = View.GONE
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+    }
+}
--- app/src/main/java/com/example/fintrack/activities/SelectBudget.kt
+++ app/src/main/java/com/example/fintrack/activities/SelectBudget.kt
@@ -1,23 +1,199 @@
 package com.example.fintrack.activities
-//
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.core.app.NotificationCompat
+import com.example.fintrack.R
+import kotlinx.android.synthetic.main.activity_select_budget.*
+
+class SelectBudget : AppCompatActivity() {
+
+    var startPoint = 0
+    var endPoint = 0
+    var num = 0
+
+
+    companion object {
+        private const val CHANNEL_ID = "BudgetNotificationChannel"
+    }
+
+    //private val CHANNEL_ID = "BudgetNotification"
+    private val NOTIFICATION_ID = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_budget)
+
+        val ivbackwaredSB = findViewById<ImageView>(R.id.ivbackwaredSB)
+
+
+        ivbackwaredSB.setOnClickListener {
+            val intent = Intent(this, NewBudget::class.java)
+            startActivity(intent)
+        }
+
+        val days = ArrayList<String>()
+        for (i in 1..30) {
+            days.add(i.toString())
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, days)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spDaysSB.adapter = adapter
+
+        spDaysSB.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedDay = parent.getItemAtPosition(position).toString()
+                // Do something with the selected day
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing
+            }
+        }
+
+
+
+        sbExpenseSB.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                lbExpenseValueSB.text = "Rs " + progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+
+            }
+
+
+        })
+
+        sbSavingsSB.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                lbSavingsValueSB.text = "Rs " + progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+
+            }
+
+
+        })
+
+
+
+        btnincrementSB.setOnClickListener {
+            num++
+
+            lbUsersSB.text = num.toString()
+        }
+
+        btndecrementSB.setOnClickListener {
+            num--
+
+            lbUsersSB.text = num.toString()
+        }
+
+        btnconfirmSB.setOnClickListener {
+            val intent = Intent(this, NewBudget::class.java)
+            intent.putExtra("expense", sbExpenseSB.progress)
+            intent.putExtra("savings", sbSavingsSB.progress)
+            intent.putExtra("days", spDaysSB.selectedItem.toString().toInt())
+            intent.putExtra("users", num)
+            startActivity(intent)
+//            showToast("Budget successfully created")
+            showNotification("Budget successfully created")
+
+        }
+
+
+    }
+
+    private fun showNotification(message: String) {
+        createNotificationChannel()
+
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.notification_icon)
+            .setContentTitle("Budget Notification")
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = "Budget Channel"
+            val channelDescription = "Channel for budget notifications"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, channelName, importance)
+            channel.description = channelDescription
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+//    private fun showToast(message: String) {
+//        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+//    }
+    }
+
+
+// SelectBudget Activity
+//package com.example.fintrack.activities
+//import android.content.Intent
 //import androidx.appcompat.app.AppCompatActivity
 //import android.os.Bundle
 //import android.view.View
 //import android.widget.AdapterView
 //import android.widget.ArrayAdapter
+//import android.widget.ImageView
 //import android.widget.SeekBar
 //import com.example.fintrack.R
+//import com.example.fintrack.activities.NewBudget
 //import kotlinx.android.synthetic.main.activity_select_budget.*
 //
 //class SelectBudget : AppCompatActivity() {
 //
-//    var startPoint = 0
-//    var endPoint = 0
-//    var num = 0
+//    private var expenseValue = 0
+//    private var savingValue = 0
+//    private var days = 0
+//    private var users = 0
 //
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_select_budget)
+//
+//        val ivbackwaredSB = findViewById<ImageView>(R.id.ivbackwaredSB)
+//
+//        ivbackwaredSB.setOnClickListener{
+//            val intent= Intent(this, NewBudget::class.java)
+//            startActivity(intent)
+//        }
 //
 //        val days = ArrayList<String>()
 //        for (i in 1..30) {
@@ -30,8 +206,7 @@ package com.example.fintrack.activities
 //
 //        spDaysSB.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
 //            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
-//                val selectedDay = parent.getItemAtPosition(position).toString()
-//                // Do something with the selected day
+//                days = parent.getItemAtPosition(position).toString().toInt()
 //            }
 //
 //            override fun onNothingSelected(parent: AdapterView<*>) {
@@ -39,55 +214,64 @@ package com.example.fintrack.activities
 //            }
 //        }
 //
-//
-//
-//        sbExpenseSB.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+//        sbExpenseSB.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
 //            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-//                lbExpenseValueSB.text = "Rs " + progress.toString()
+//                expenseValue = progress
+//                lbExpenseValueSB.text = "Rs $expenseValue"
 //            }
 //
 //            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-//
+//                // Do nothing
 //            }
 //
 //            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-//
-//
+//                // Do nothing
 //            }
-//
-//
 //        })
 //
-//        sbSavingsSB.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+//        sbSavingsSB.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
 //            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-//                lbSavingsValueSB.text = "Rs " + progress.toString()
+//                savingValue = progress
+//                lbSavingsValueSB.text = "Rs $savingValue"
 //            }
 //
 //            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-//
+//                // Do nothing
 //            }
 //
 //            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-//
-//
+//                // Do nothing
 //            }
-//
-//
 //        })
-//
-//
 //
 //        btnincrementSB.setOnClickListener {
-//            num++
-//
-//            lbUsersSB.text = num.toString()
+//            users++
+//            lbUsersSB.text = users.toString()
 //        }
 //
 //        btndecrementSB.setOnClickListener {
-//            num--
-//
-//            lbUsersSB.text = num.toString()
+//            if (users > 0) {
+//                users--
+//                lbUsersSB.text = users.toString()
+//            }
 //        }
 //
+//        btnconfirmSB.setOnClickListener {
+//            val intent = Intent(this, NewBudget::class.java).apply {
+//                putExtra("expenseValue", expenseValue)
+//                putExtra("savingValue", savingValue)
+//                putExtra("days", days)
+//                putExtra("users", users)
+//            }
+//            startActivity(intent)
+//        }
 //    }
 //}
+
+
+
+
+
+
+
+
